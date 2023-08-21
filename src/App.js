b4ceb28af8c33c81a6ca4fb969f782a81b6780be
sarahruthmik <128433888+sarahruthmik@users.noreply.github.com>
2023-08-21 13:46:18 -0400

    Added footer with link to code

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather Application</h1>
+      <footer>
+        This project was coded by Sarah Mikulski and is{" "}
+        <a
+          href="https://github.com/sarahruthmik/react-weather-app.git"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
