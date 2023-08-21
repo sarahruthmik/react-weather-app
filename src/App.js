ad3a50f25b1a61b8b0d152f141ba01d336b86090
sarahruthmik <128433888+sarahruthmik@users.noreply.github.com>
2023-08-21 14:16:19 -0400

    Added bootstrap and weather component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Application</h1>
-      <footer>
-        This project was coded by Sarah Mikulski and is{" "}
-        <a
-          href="https://github.com/sarahruthmik/react-weather-app.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather Application</h1>
+        <Weather />
+        <footer>
+          This project was coded by Sarah Mikulski and is{" "}
+          <a
+            href="https://github.com/sarahruthmik/react-weather-app.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
