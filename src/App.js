b29d6343ae2162ebc49dac2b43448e36e7d39eb8
sarahruthmik <128433888+sarahruthmik@users.noreply.github.com>
2023-08-22 10:26:01 -0400

    Added basic css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import "./App.css";
 import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather Application</h1>
         <Weather />
         <footer>
           This project was coded by Sarah Mikulski and is{" "}
