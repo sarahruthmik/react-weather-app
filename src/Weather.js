4e3153fc3169bf2a7c5699129123c4767e93834a
sarahruthmik <128433888+sarahruthmik@users.noreply.github.com>
2023-08-22 12:00:04 -0400

    Added defaultCity

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
     console.log(response.data);
@@ -73,8 +73,7 @@ export default function Weather() {
     );
   } else {
     const apiKey = "4f86aea83o0bad32b69a00tb6fb3bd19";
-    let city = "New York";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
     return "Loading...";
   }
